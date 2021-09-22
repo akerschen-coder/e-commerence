cd7c571180c906b75398d87cb5c7974a0e530736
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-09-22 14:18:58 -0600

    seeding and password material

--- models/index.js
+++ models/index.js
@@ -3,18 +3,17 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { triggerAsyncId } = require('async_hooks');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'SET NULL',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'SET NULL',
 });
 
 // Products belongToMany Tags (through ProductTag)
